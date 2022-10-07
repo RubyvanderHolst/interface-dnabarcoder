e7e2b856156b82893932ce44d8b68b7266efa9b0
Ruby van der Holst <s1124474@student.hsleiden.nl>
2022-10-07 14:39:55 +0200

    uploaded files are saved and can be used by dnabarcoder, output files can be generated, celery added to requirements.txt

--- apps/Interface/views.py
+++ apps/Interface/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 import numpy as np
+from celery import task
 import os
 
 
@@ -13,8 +14,26 @@ def cutoff_page(request):
 
 def cutoff_results_page(request):
     test = request.POST
+
+    dnabarcoder_path = "/home/tool/dnabarcoder.py"
+    input_bestand_path = request.FILES['input_reference'].temporary_file_path()
+    rank = request.POST['rank'][0]
+    higher_rank = request.POST['higher_rank'][0]
+
+    output = os.popen(f"python {dnabarcoder_path} "
+                      f"predict "
+                      f"--input {input_bestand_path} "
+                      f"--startingthreshold 0.7 "
+                      f"--endthreshold 1 "
+                      f"--step 0.001 "
+                      f"-rank genus "
+                      f"-higherrank family "
+                      f"--minalignmentlength 10 "
+                      f"-prefix cutoff_result "
+                      f"-o /home/app/static/results ").read()
     return render(request, 'cutoff_results.html', {
         'test': test,
+        'output': output,
     })
 
 
@@ -39,4 +58,3 @@ def classification_results_page(request):
 
 def visualization_page(request):
     return render(request, 'visualization.html')
-
