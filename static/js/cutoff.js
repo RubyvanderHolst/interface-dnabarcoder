749461731c7b4e7120179ffd0b27bd063fe19df1
Ruby van der Holst <s1124474@student.hsleiden.nl>
2022-10-18 13:50:42 +0200

    "All" is selected for higher_rank when global cutoff is selected and restructuring of cutoff.js

--- static/js/cutoff.js
+++ static/js/cutoff.js
@@ -1,37 +1,34 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     // Enable/disable higher rank
+        let rank_dropdown = document.getElementById('id_rank')
+    let higher_rank_dropdown = document.getElementById('id_higher_rank')
+    let all_rank = ['species', 'genus', 'family', 'order', 'class', 'phylum', 'kingdom']
+
     let cutoff_radio = Array.from(
         document.querySelectorAll('input[name="cutoff_type"]')
     )
-    let higher_rank_dropdown = document.getElementById('id_higher_rank')
+
     for (let i = 0; i < cutoff_radio.length; i++) {
         let radiobutton = cutoff_radio[i]
         radiobutton.addEventListener('click', function () {
             if (radiobutton.value === 'local') {
                 higher_rank_dropdown.disabled = false;
+                higher_rank_dropdown[higher_rank_dropdown.length-1].hidden = true
+                set_higher(rank_dropdown, higher_rank_dropdown, all_rank)
             } else if (radiobutton.value === 'global') {
                 higher_rank_dropdown.disabled = true;
-                higher_rank_dropdown.value = "";
+                higher_rank_dropdown[higher_rank_dropdown.length-1].hidden = false
+                higher_rank_dropdown.value = 'all';
             }
         })
     }
 
     // Hide higher rank option that are the same or lower than the selected rank
-    let rank_dropdown = document.getElementById('id_rank')
     rank_dropdown.addEventListener('change', function() {
         if (!higher_rank_dropdown.disabled) {
-            let all_rank = ['species', 'genus', 'family', 'order', 'class', 'phylum', 'kingdom']
-            let selected_rank = rank_dropdown.value
-            let index = all_rank.findIndex(object => {
-                return object === selected_rank
-            })
 
-            if (index === all_rank.length-1) {
-                higher_rank_dropdown.value = all_rank[index]
-            } else {
-                higher_rank_dropdown.value = all_rank[index + 1]
-            }
+            let index = set_higher(rank_dropdown, higher_rank_dropdown, all_rank)
 
             for (let i = 0; i < index; i++) {
                 higher_rank_dropdown[i].hidden = true
@@ -50,3 +47,21 @@ document.addEventListener('DOMContentLoaded', function() {
         cutoff_value.required = remove_comp_check.checked
     })
 })
+
+// Sets higher_rank to one higher than selected at rank
+// Returns index of selected rank in all_rank array
+function set_higher(rank_dropdown, higher_rank_dropdown, all_rank) {
+    let selected_rank = rank_dropdown.value
+
+    let index = all_rank.findIndex(object => {
+        return object === selected_rank
+    })
+
+    if (index === all_rank.length-1) {
+        higher_rank_dropdown.value = all_rank[index]
+    } else {
+        higher_rank_dropdown.value = all_rank[index + 1]
+    }
+
+    return index
+}
