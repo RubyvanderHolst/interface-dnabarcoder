30f386b817c8702916c08a47aacb0e6b0d051178
Ruby van der Holst <s1124474@student.hsleiden.nl>
2022-10-27 15:35:53 +0200

    seperated prediction into tasks.py and created a loading page. Data generation moved from html to js

--- apps/Interface/tasks.py
+++ apps/Interface/tasks.py
@@ -1,2 +1,59 @@
 from celery import shared_task
+import os
 
+
+@shared_task
+def calculate_cutoff(dnabarcoder_path, input_file_path, sim_file_path,
+                     min_alignment_length, rank, higher_rank,
+                     starting_threshold,
+                     end_threshold, step, min_group_number, min_seq_number,
+                     max_seq_number, rem_comp_1, prefix, output_dir):
+    command = f"python {dnabarcoder_path} " \
+              f"predict " \
+              f"--input {input_file_path} " \
+              f"--simfilename {sim_file_path} " \
+              f"--minalignmentlength {min_alignment_length} " \
+              f"-rank {rank} " \
+              f"-higherrank {higher_rank} " \
+              f"--startingthreshold {starting_threshold} " \
+              f"--endthreshold {end_threshold} " \
+              f"--step {step} " \
+              f"-mingroupno {min_group_number} " \
+              f"-minseqno {min_seq_number} " \
+              f"-maxseqno {max_seq_number} " \
+              f"-removecomplexes {rem_comp_1} " \
+              f"-prefix {prefix} " \
+              f"--out {output_dir} "
+
+    os.system(command)
+
+    dict_files, dict_images = get_file_sizes(output_dir)
+
+    return dict_files, dict_images
+# TODO add task remove complexes
+
+
+def get_file_sizes(dir_path):
+    # Get the sizes of all files in a directory
+    # Return two dictionaries: {file_name: file_size}
+    # one for images and one for all other files
+    file_list = os.listdir(dir_path)
+    dict_files = {}
+    dict_images = {}
+    for name in file_list:
+        size = bytes_to_larger(os.stat(os.path.join(dir_path, name)).st_size)
+        if name[-4:] == '.png':
+            dict_images[name] = size
+        else:
+            dict_files[name] = size
+    return dict_files, dict_images
+
+
+def bytes_to_larger(size_b):
+    sizes = ['TB', 'GB', 'MB', 'KB', 'B']
+    x = 10**((len(sizes) - 1) * 3)
+    for size in sizes:
+        if size_b > x:
+            return f"{int(size_b / x)} {size}"
+        else:
+            x *= 10**-3
--- static/js/cutoff_results.js
+++ static/js/cutoff_results.js
@@ -1,10 +1,71 @@
-document.addEventListener('load', function() {
-    document.getElementById('loading').style.display = "none";
-    document.getElementById('done').style.display = "block";
-})
+document.addEventListener('DOMContentLoaded', function () {
+     let task_id = document.getElementById('task_id').innerText
 
-// learn to use ajax to get data afterwards
-// https://stackoverflow.com/questions/59510573/how-can-i-show-a-loading-animation-in-django-while-the-server-is-processing-a-vi
-// https://www.geeksforgeeks.org/handling-ajax-request-in-django/
-// Using jQuery will make it significantly easier
-// https://stackoverflow.com/questions/8567114/how-can-i-make-an-ajax-call-without-jquery
+     get_data()
+
+     function get_data() {
+          $.ajax({
+               url: task_id,
+               type: "GET",
+               dataType: "json",
+               success: (data) => {
+                    document.getElementById('task_status').innerHTML = data.state
+                    if (data.state === 'SUCCESS'){
+                        document.getElementById('spinner-box').style.display = "none";
+                         document.getElementById('data-box').style.display = "block";
+
+                         for (const [file_name, file_size] of Object.entries(data.files)) {
+                              let tr = "<tr>";
+                              tr += `<td>
+                                        ${file_name}
+                                     </td>
+                                     <td>
+                                        ${file_size}
+                                     </td>
+                                     <td>
+                                        <button type='button' class='btn btn-success w-100'>
+                                            <a href='/media/cutoff/${file_name}' class='link-light text-decoration-none' download="${file_name}">
+                                                Download
+                                            </a>
+                                        </button>
+                                     </td></tr>`
+                             document.getElementById('tbody-base').innerHTML += tr;
+                         }
+
+                        for (const [image_name, image_size] of Object.entries(data.images)) {
+                            let image_card =
+                                `<div class="card mx-auto w-50">
+                                    <h5 class="card-header text-center">${image_name} (${image_size})</h5>
+                                    <div class="card-body text-center">
+                                        <img
+                                            src="/media/cutoff/${image_name}"
+                                            class="img-fluid">
+                                            <button type="button"
+                                                    class="btn btn-success">
+                                                <a href="/media/cutoff/${image_name}"
+                                                   class="link-light text-decoration-none"
+                                                   download>
+                                                    Download
+                                                </a>
+                                            </button>
+                                    </div>
+                                </div>`
+                            document.getElementById('images_div').innerHTML += image_card
+                        }
+
+                    } else {
+                         setTimeout(function(){
+                              get_data();
+                              }, 1000) // Wait 1 second until reload
+                    }
+                    },
+               error: (error) => {
+                    console.log('error', error)
+                    document.getElementById('data-box').innerHTML = 'An error has occurred'
+                    setTimeout(function(){
+                         get_data();
+                         }, 1000) // Wait 1 seconds
+                    }
+               })
+     }
+})
